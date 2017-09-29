12595f47171884522eb9e6e55e6324aee576297d
yohanstra <jreinstra@gmail.com>
2017-09-28 23:58:26 -0700

    Finish first draft of models

--- apps/reader/models.py
+++ apps/reader/models.py
@@ -2,21 +2,37 @@ from django.db import models
 
 from django.contrib.auth.models import User
 from apps.admin import Hackathon, Organization
+from apps.registration import Application, Applicant
 
 
 class Reader(models.Model):
-	user = models.OneToOneField(User, related_name="reader")
-	organization = models.ForeignKey(Organization, related_name="readers")
-	hackathons = models.ManyToManyField(Hackathon, related_name="readers")
+    user = models.OneToOneField(User, related_name="reader")
+    organization = models.ForeignKey(Organization, related_name="readers")
+    hackathons = models.ManyToManyField(Hackathon, related_name="readers")
 
 
 class Rating(models.Model):
-	pass
+    application = models.OneToOneField(Application, related_name="rating")
 
 
 class RatingField(models.Model):
-	pass
+    TYPE_NUMERICAL = "numerical"
+    TYPE_MULTIPLE_CHOICE = "multiple_choice"
+
+    TYPE_CHOICES = (
+        (TYPE_NUMERICAL, TYPE_NUMERICAL),
+        (TYPE_MULTIPLE_CHOICE), (TYPE_MULTIPLE_CHOICE),
+    )
+
+    rating = modoels.ForeignKey(Rating, related_name="fields")
+    type = models.CharField(max_length=16, choices=TYPE_CHOICES)
+    prompt = models.CharField(max_length=64)
+    min_number = models.IntegerField(default=-1)
+    max_number = models.IntegerField(default=-1)
+    options = models.TextField(default="")
 
 
 class RatingResponse(models.Model):
-	pass
+    reader = models.ForeignKey(Reader, related_name="ratings")
+    applicant = models.ForeignKey(Applicant, related_name="ratings")
+    data = models.TextField()
--- apps/registration/models.py
+++ apps/registration/models.py
@@ -1,17 +1,54 @@
 from django.db import models
 
-
-class Applicant(models.Model):
-	pass
+from django.contrib.auth.models import User
+from apps.admin import Hackathon
 
 
 class Application(models.Model):
-	pass
+    STATUS_PREPARING = "preparing"
+    STATUS_OPEN = "open"
+    STATUS_CLOSED = "closed"
+
+    STATUS_CHOICES = (
+        (STATUS_PREPARING, STATUS_PREPARING),
+        (STATUS_OPEN, STATUS_OPEN),
+        (STATUS_CLOSED, STATUS_CLOSED),
+    )
+
+    name = models.CharField(max_length=128)
+    status = models.CharField(max_length=16, choices=STATUS_CHOICES)
+    hackathon = models.ForeignKey(Hackathon, related_name="applications")
 
 
 class ApplicationField(models.Model):
-	pass
+    TYPE_MULTIPLE_CHOICE = "multiple_choice"
+    TYPE_MULTIPLE_CHOICE_OTHER = "multiple_choice_other"
+    TYPE_SHORT_ANSWER = "short_answer"
+    TYPE_LONG_ANSWER = "long_answer"
+
+    TYPE_CHOICES = (
+        (TYPE_MULTIPLE_CHOICE, TYPE_MULTIPLE_CHOICE),
+        (TYPE_MULTIPLE_CHOICE_OTHER, TYPE_MULTIPLE_CHOICE_OTHER),
+        (TYPE_SHORT_ANSWER, TYPE_SHORT_ANSWER),
+        (TYPE_LONG_ANSWER, TYPE_LONG_ANSWER),
+    )
+
+    application = models.ForeignKey(Application, related_name="fields")
+    ordering = models.IntegerField()
+    type = models.CharField(max_length=32, choices=TYPE_CHOICES)
+    prompt = models.CharField(max_length=256)
+    options = models.TextField(default="")
 
 
-class ApplicationResponse(models.Model):
-	pass
+class ApplicantTeam(models.Model):
+    hackathon = models.ForeignKey(Hackathon)
+    name = models.CharField(max_length=64)
+    entry_code = models.CharField(max_length=64)
+
+
+class Applicant(models.Model):
+    user = models.OneToOneField(User, related_name="applicant")
+    hackathon = models.ForeignKey(Hackathon, related_name="applicants")
+    application = models.ForeignKey(Application, related_name="applicants")
+    team = models.ForeignKey(ApplicantTeam, related_name="applicants", blank=True, null=True)
+    data = models.TextField()
