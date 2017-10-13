f54a7d44b4a1a4b1a1e62a3e2342cff2929b50b2
yohanstra <jreinstra@gmail.com>
2017-10-13 00:53:43 -0700

    revert wsgi

--- madras/wsgi.py
+++ madras/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "madras.settings")
 
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
