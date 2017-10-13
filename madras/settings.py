5faffea5a944985c6b8ef6c5395113f18b2d6ad8
yohanstra <jreinstra@gmail.com>
2017-10-13 01:38:08 -0700

    whitenoise

--- madras/settings.py
+++ madras/settings.py
@@ -136,6 +136,11 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # SECURE_SSL_REDIRECT = True
 
 try:
