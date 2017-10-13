2b1756de888b3e0bb8c79e2f3d230fc2273cd166
yohanstra <jreinstra@gmail.com>
2017-10-13 00:46:26 -0700

    new settings

--- madras/settings.py
+++ madras/settings.py
@@ -115,6 +115,28 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Heroku settings below
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+SECURE_SSL_REDIRECT = True
+
 try:
     from local_settings import *
 except ImportError:
