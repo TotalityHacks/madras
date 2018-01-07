55fd1ccd2f17987e014285bb243dbeca6429f3f1
Eric Wang <ezwang1@gmail.com>
2018-01-07 11:47:39 -0500

    better check for nonexistent github user

--- apps/reader/utils.py
+++ apps/reader/utils.py
@@ -1,5 +1,7 @@
 import requests
+
 from github3 import login
+from github3.null import NullObject
 
 
 def get_contributions(github_user_name):
@@ -15,20 +17,20 @@ def get_contributions(github_user_name):
 def get_metrics_github(github_user_name):
     gh = login("GITHUB_USER_NAME", password="GITHUB_PASSWORD")
     user = gh.user(github_user_name)
-    try:
-        string_user_null = str(user.is_null)
-        if string_user_null == "<bound method NullObject.is_null of <NullObject('User')>>":
-            return {}
-    except AttributeError:
-        user_repos = gh.repositories_by(github_user_name)
-        user_starred = gh.starred_by(github_user_name)
-        count_of_valid_repos = 0
-        for repo in user_repos:
-            count_of_valid_repos += 1
-        star_repos = set(list(user_repos)).intersection(list(user_starred))
-        return {
-            "num_followers": user.followers_count,
-            "num_repos": count_of_valid_repos,
-            "num_contributions": get_contributions(github_user_name),
-            "self_star_repos": len(star_repos),
-        }
+
+    # If no user exists, return an empty dictionary.
+    if isinstance(user, NullObject):
+        return {}
+
+    user_repos = gh.repositories_by(github_user_name)
+    user_starred = gh.starred_by(github_user_name)
+    count_of_valid_repos = 0
+    for repo in user_repos:
+        count_of_valid_repos += 1
+    star_repos = set(list(user_repos)).intersection(list(user_starred))
+    return {
+        "num_followers": user.followers_count,
+        "num_repos": count_of_valid_repos,
+        "num_contributions": get_contributions(github_user_name),
+        "self_star_repos": len(star_repos),
+    }
