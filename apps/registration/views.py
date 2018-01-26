b396871dfdb919edfbde617a16ab5b1bec12d122
mike2151 <redefiningproductions@gmail.com>
2018-01-26 00:50:36 -0500

    started registration

--- apps/registration/views.py
+++ apps/registration/views.py
@@ -1,39 +1,57 @@
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from django.shortcuts import get_object_or_404
-
-from apps.registration import models, serializers
-
-
-class Application(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request):
-        applicant = get_object_or_404(models.Applicant, user=request.user)
-
-        return Response(
-            serializers.ApplicationSchemaSerializer(applicant.application).data,
-            status=status.HTTP_200_OK,
-        )
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 
 
-class Applicant(APIView):
-    permission_classes = (IsAuthenticated,)
+def home(request):
+    return render(request, 'home.html')
 
-    def get(self, request):
-        applicant = get_object_or_404(models.Applicant, user=request.user)
 
-        return Response(
-            serializers.ApplicantSerializer(applicant).data,
-            status=status.HTTP_200_OK,
-        )
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            message = render_to_string('acc_active_email.html', {
+                'user':user, 'domain':current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            # Sending activation link in terminal
+            # user.email_user(subject, message)
+            mail_subject = 'Activate your blog account.'
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration.')
+            # return render(request, 'acc_active_sent.html')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
 
-    def post(self, request):
-        serializer = serializers.ApplicantSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
