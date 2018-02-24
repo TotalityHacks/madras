3ef5a7e5b88db7f3d1ba70d48df9be897d6f93c7
Eric Wang <ezwang1@gmail.com>
2018-02-23 19:26:35 -0500

    temporarily disable test

--- apps/reader/tests.py
+++ apps/reader/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
+from unittest import skip
 
 from .utils import get_metrics_github
 
 
 class UtilsTests(TestCase):
+
+    @skip("This test reaches the GitHub API request limit too easily.")
     def test_github_metrics(self):
         metrics = get_metrics_github("ezwang")
         self.assertEquals(len(metrics), 4)
