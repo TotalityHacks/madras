d6a57fa7ae7cd4a0789f2131cbc5677d00aa82cb
thomas <thomas.woodside@gmail.com>
2018-03-25 09:18:56 -0700

    made check in and check out into actual requests

--- apps/checkin/urls.py
+++ apps/checkin/urls.py
@@ -4,4 +4,5 @@ from apps.checkin import views
 urlpatterns = [
     url(r'^get_qr_code/$', views.get_qr_code, name="get_qr_code"),
     url(r'^get_qr_codes/$', views.get_qr_codes, name="get_qr_codes"),
+    url(r'^check_in/$', views.check_in, name="get_qr_codes")
 ]
