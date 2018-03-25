fb0804163695dacbaa66b435c0e18668d0d40863
thomas <thomas.woodside@gmail.com>
2018-03-25 09:19:41 -0700

    added urls for check in and out

--- apps/checkin/urls.py
+++ apps/checkin/urls.py
@@ -4,5 +4,6 @@ from apps.checkin import views
 urlpatterns = [
     url(r'^get_qr_code/$', views.get_qr_code, name="get_qr_code"),
     url(r'^get_qr_codes/$', views.get_qr_codes, name="get_qr_codes"),
-    url(r'^check_in/$', views.check_in, name="get_qr_codes")
+    url(r'^check_in/$', views.check_in, name="check_in"),
+    url(r'^check_out/$', views.check_out, name="check_out"),
 ]
