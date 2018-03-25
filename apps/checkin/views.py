1ab93a549a631448ce2f5a0fb01d452210bb17d6
thomas <thomas.woodside@gmail.com>
2018-03-25 09:19:41 -0700

    added urls for check in and out

--- apps/checkin/views.py
+++ apps/checkin/views.py
@@ -22,14 +22,21 @@ static_path = "static/checkin/qr_codes/"
 def get_qr_code(request):
     if request.GET:
         try:
-            email = request.GET["email"]
-            applicant = Applicant.objects.get(email=request.GET["email"])
+            email = request.POST["email"]
+        except KeyError:
+            return error_response("Must include email.",
+                                  "You must include the user's email in the request body.",
+                                  400)
+        try:
+            applicant = Applicant.objects.get(email=email)
         except Applicant.DoesNotExist:
-            return error_response("User does not exist.", "A user with that email address was not found in the database.", 404)
+            return error_response("User does not exist.",
+                                  "A user with that email address was not found in the database.",
+                                  404)
         # TODO: check that applicant was actually admitted
         return success_data_jsonify({ "qr_image_path": "/" + return_qr(applicant.email)})
     else:
-        return error_response("Invalid method.", "Please use a get request.", 400)
+        return error_response("Invalid method.", "Please use a get request.", 405)
 
 
 def get_qr_codes(request):
@@ -51,44 +58,62 @@ def return_qr(email):
     return relative_path
 
 
-def check_in(email):
+def check_in(request):
+    if request.POST:
+        try:
+            email = request.POST["email"]
+        except KeyError:
+            return error_response("Must include email.",
+                                  "You must include the user's email in the request body.",
+                                  400)
+        try:
+            applicant = Applicant.objects.get(email=email)
+        except Applicant.DoesNotExist:
+            return error_response("User does not exist.",
+                                  "A user with that email address was not found in the database.",
+                                  404)
+        group, _ = CheckInGroup.objects.get_or_create(applicant=applicant)
+        if group.checked_in:
+            return error_response("User already checked in.",
+                                  "The user specified is already checked in.",
+                                  409)
+        event = CheckInEvent(check_in_group=group, check_in=True, time=datetime.utcnow())
+        group.checked_in = True
+        event.save()
+        group.save()
+        return success_data_jsonify()
+    else:
+        return error_response("Invalid method.", "Please use a post request.", 405)
     # TODO: will need to be authenticated to admins
-    try:
-        applicant = Applicant.objects.get(email=email)
-    except Applicant.DoesNotExist:
-        return error_response("User does not exist.",
-                              "A user with that email address was not found in the database.",
-                              404)
-    group = CheckInGroup.objects.get_or_create(applicant=applicant)
-    if group.checked_in:
-        return error_response("User already checked in.",
-                              "The user specified is already checked in.",
-                              409)
-    event = CheckInEvent(check_in_group=group, check_in=True, time=datetime.utcnow())
-    group.checked_in = True
-    event.save()
-    group.save()
-    return success_data_jsonify()
 
 
-def check_out(email):
+def check_out(request):
+    if request.POST:
+        try:
+            email = request.POST["email"]
+        except KeyError:
+            return error_response("Must include email.",
+                                  "You must include the user's email in the request body.",
+                                  400)
+        try:
+            applicant = Applicant.objects.get(email=email)
+        except Applicant.DoesNotExist:
+            return error_response("User does not exist.",
+                                  "A user with that email address was not found in the database.",
+                                  404)
+        group, _ = CheckInGroup.objects.get_or_create(applicant=applicant)
+        if not group.checked_in:
+            return error_response("User not checked in.",
+                                  "The user must be checked in to be checked out.",
+                                  409)
+        event = CheckInEvent(check_in_group=group, check_in=False, time=datetime.utcnow())
+        group.checked_in = False
+        event.save()
+        group.save()
+        return success_data_jsonify()
+    else:
+        return error_response("Invalid method.", "Please use a post request.", 405)
     # TODO: will need to be authenticated to admins
-    try:
-        applicant = Applicant.objects.get(email=email)
-    except Applicant.DoesNotExist:
-        return error_response("User does not exist.",
-                              "A user with that email address was not found in the database.",
-                              404)
-    group = CheckInGroup.objects.get_or_create(applicant=applicant)
-    if not group.checked_in:
-        return error_response("User is not checked in.",
-                              "The user must be checked in to be checked out.",
-                              409)
-    event = CheckInEvent(check_in_group=group, check_in=False, time=datetime.utcnow())
-    group.checked_in = False
-    event.save()
-    group.save()
-    return success_data_jsonify()
 
 
 # TODO: move the below functions to utils somewhere so everyone can use them
