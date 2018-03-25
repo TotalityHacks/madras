06f64904fe834b1386a9f24d455e80029d74c579
thomas <thomas.woodside@gmail.com>
2018-03-24 18:16:44 -0700

    simple user exists check

--- apps/checkin/views.py
+++ apps/checkin/views.py
@@ -7,6 +7,12 @@ from django.core.mail import EmailMessage
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import get_user_model
 from madras import settings
+from apps.registration.models import Applicant
 
 def gen_qr_code(request):
-    print(request.GET["email"])
+    try:
+        applicant = Applicant.objects.get(email=request.GET["email"])
+    except Applicant.DoesNotExist:
+        return JsonResponse({"success": False, "err_field": "User does not exist."})
+    # TODO: check that applicant was actually admitted
+
