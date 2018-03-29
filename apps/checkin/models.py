4a3cb20166efd1e7103af2a847bcbe1efb1b5f23
thomas <thomas.woodside@gmail.com>
2018-03-29 06:56:14 -0700

    fixed strange POST thing I did on accident, replaced email with uuid in qr codes

--- apps/checkin/models.py
+++ apps/checkin/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from apps.registration.models import Applicant
+import uuid
 
 
 class CheckInGroup(models.Model):
     checked_in = models.BooleanField(default=False)
 
     applicant = models.ForeignKey(Applicant)
+
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
 
     def __str__(self):
         return self.applicant.__str__() + " checked in" if self.checked_in else " not checked in"
--- apps/checkin/views.py
+++ apps/checkin/views.py
@@ -14,7 +14,7 @@ static_path = "static/checkin/qr_codes/"
 def get_qr_code(request):
     if request.GET:
         try:
-            email = request.POST["email"]
+            email = request.GET["email"]
         except KeyError:
             return error_response("Must include email.",
                                   "You must include the user's email in the request body.",
@@ -26,7 +26,9 @@ def get_qr_code(request):
                                   "A user with that email address was not found in the database.",
                                   404)
         # TODO: check that applicant was actually admitted
-        return success_data_jsonify({ "qr_image_path": "/" + return_qr(applicant.email)})
+        group = CheckInGroup(applicant=applicant)
+        group.save()
+        return success_data_jsonify({ "qr_image_path": "/" + return_qr(group.uuid)})
     else:
         return error_response("Invalid method.", "Please use a get request.", 405)
 
@@ -37,16 +39,18 @@ def get_qr_codes(request):
     # TODO: filter to only include admitted applicants
     files = []
     for applicant in applicants:
-        files.append(return_qr(applicant.email))
+        group = CheckInGroup(applicant=applicant)
+        group.save()
+        files.append(return_qr(group.uuid))
     return success_data_jsonify({"qr_image_paths": files})
 
 
-def return_qr(email):
-    relative_path = static_path + email.replace("@", "").replace(".", "") + ".png"
+def return_qr(uuid):
+    relative_path = static_path + uuid + ".png"
     full_path = os.path.join(settings.PROJECT_ROOT, relative_path)
     if not os.path.exists(full_path):
         with open(full_path, "wb") as f:
-            qrcode.make(email).save(f, format="PNG")
+            qrcode.make(uuid).save(f, format="PNG")
     return relative_path
 
 
@@ -54,18 +58,17 @@ def return_qr(email):
 def check_in(request):
     if request.POST:
         try:
-            email = request.POST["email"]
+            uuid = request.POST["uuid"]
         except KeyError:
-            return error_response("Must include email.",
-                                  "You must include the user's email in the request body.",
+            return error_response("Must include uuid.",
+                                  "You must include the user's uuid from the bar code in the request body.",
                                   400)
         try:
-            applicant = Applicant.objects.get(email=email)
-        except Applicant.DoesNotExist:
-            return error_response("User does not exist.",
-                                  "A user with that email address was not found in the database.",
+            group = CheckInGroup.objects.get(uuid=uuid)
+        except CheckInGroup.DoesNotExist:
+            return error_response("uuid does not exist",
+                                  "The uuid you specified does not exist in the database.",
                                   404)
-        group, _ = CheckInGroup.objects.get_or_create(applicant=applicant)
         if group.checked_in:
             return error_response("User already checked in.",
                                   "The user specified is already checked in.",
@@ -83,31 +86,31 @@ def check_in(request):
 @csrf_exempt
 def check_out(request):
     if request.POST:
-        try:
-            email = request.POST["email"]
-        except KeyError:
-            return error_response("Must include email.",
-                                  "You must include the user's email in the request body.",
-                                  400)
-        try:
-            applicant = Applicant.objects.get(email=email)
-        except Applicant.DoesNotExist:
-            return error_response("User does not exist.",
-                                  "A user with that email address was not found in the database.",
-                                  404)
-        group, _ = CheckInGroup.objects.get_or_create(applicant=applicant)
-        if not group.checked_in:
-            return error_response("User not checked in.",
-                                  "The user must be checked in to be checked out.",
-                                  409)
-        event = CheckInEvent(check_in_group=group, check_in=False, time=timezone.now())
-        group.checked_in = False
-        event.save()
-        group.save()
-        return success_data_jsonify()
-    else:
-        return error_response("Invalid method.", "Please use a post request.", 405)
-    # TODO: will need to be authenticated to admins
+        if request.POST:
+            try:
+                uuid = request.POST["uuid"]
+            except KeyError:
+                return error_response("Must include uuid.",
+                                      "You must include the user's uuid from the bar code in the request body.",
+                                      400)
+            try:
+                group = CheckInGroup.objects.get(uuid=uuid)
+            except CheckInGroup.DoesNotExist:
+                return error_response("uuid does not exist",
+                                      "The uuid you specified does not exist in the database.",
+                                      404)
+            if not group.checked_in:
+                return error_response("User is not checked in.",
+                                      "A user must be checked in to be checked out.",
+                                      409)
+            event = CheckInEvent(check_in_group=group, check_in=False, time=timezone.now())
+            group.checked_in = False
+            event.save()
+            group.save()
+            return success_data_jsonify()
+        else:
+            return error_response("Invalid method.", "Please use a post request.", 405)
+        # TODO: will need to be authenticated to admins
 
 
 # TODO: move the below functions to utils somewhere so everyone can use them
