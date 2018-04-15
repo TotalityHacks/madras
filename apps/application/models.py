52fd34df805d600002b72db82ca985e652b5ed91
Eric Wang <ezwang1@gmail.com>
2018-04-15 18:42:49 -0400

    add question and answer models

--- apps/application/models.py
+++ apps/application/models.py
@@ -6,3 +6,13 @@ class Application(models.Model):
     github_username = models.CharField(max_length=39, blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     submission_date = models.DateTimeField(auto_now_add=True)
+
+
+class Question(models.Model):
+    text = models.TextField()
+
+
+class Answer(models.Model):
+    application = models.ForeignKey(Application)
+    question = models.ForeignKey(Question)
+    text = models.TextField()
