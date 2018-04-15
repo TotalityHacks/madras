481f56b36bbc6075c526c447394d5e90889e3271
Eric Wang <ezwang1@gmail.com>
2018-04-15 18:56:09 -0400

    fix style

--- apps/application/urls.py
+++ apps/application/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^submit/', views.ApplicationView.as_view(), name='submit'),
     url(r'^questions/$', views.QuestionView.as_view({'get': 'list'})),
-    url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionView.as_view({'get': 'retrieve', 'post': 'create', 'put': 'update', 'delete': 'destroy'}), name='question'),
+    url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionView.as_view({
+        'get': 'retrieve',
+        'post': 'create',
+        'put': 'update',
+        'delete': 'destroy'
+    }), name='question'),
 ]
