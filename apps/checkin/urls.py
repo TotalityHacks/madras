999952f47d1c293020ea12704fc4b962ac65a2a2
Thomas Woodside <thomas.woodside@gmail.com>
2018-04-29 11:32:48 -0700

    removed multiple QR getting from checkin

--- apps/checkin/urls.py
+++ apps/checkin/urls.py
@@ -3,7 +3,7 @@ from apps.checkin import views
 
 urlpatterns = [
     url(r'^get_qr_code/$', views.GetQRCode.as_view(), name="get_qr_code"),
-    url(r'^get_qr_codes/$', views.get_qr_codes, name="get_qr_codes"),
+    # url(r'^get_qr_codes/$', views.get_qr_codes, name="get_qr_codes"),
     url(r'^check_in/$', views.check_in, name="check_in"),
     url(r'^check_out/$', views.check_out, name="check_out"),
 ]
