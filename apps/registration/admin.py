4587ea744f4a9378aeea631cd2ee6ad3c1ca0865
Eric Wang <ezwang1@gmail.com>
2018-05-09 13:35:00 -0400

    better superuser interface

--- apps/registration/admin.py
+++ apps/registration/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.User)
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('email',)
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser')
+
+
+admin.site.register(models.User, UserAdmin)
