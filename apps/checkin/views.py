fd7c62b832b971a853d61ec9ad2f022525e44957
Thomas Woodside <thomas.woodside@gmail.com>
2018-05-13 16:43:43 -0700

    remove madras admin from main repo; add admin authentication for checkin

--- apps/checkin/views.py
+++ apps/checkin/views.py
@@ -5,7 +5,7 @@ import qrcode
 from django.utils import timezone
 from .models import Applicant
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.views import APIView
 from django.http import HttpResponse
 
@@ -21,59 +21,30 @@ class GetQRCode(APIView):
         group.save()
         return HttpResponse(qrcode.make(group.uuid), content_type="image/png")
 
-#
-def get_qr_code(request):
-    email = request.GET["email"]
-    # TODO: will need to be authenticated to admins
-    try:
-        applicant = Applicant.objects.get(email=email)
-    except Applicant.DoesNotExist:
-        return error_response("User does not exist.",
-                              "There is no user with this name in the database.",
-                              404)
-    # TODO: filter to only include admitted applicants
-    try:
-        group = CheckInGroup.objects.get(applicant=applicant)
-    except CheckInGroup.DoesNotExist:
-        group = CheckInGroup(applicant=applicant)
-        group.save()
-    return HttpResponse(qrcode.make(group.uuid), content_type="image/png")
 
-
-
-
-@csrf_exempt
-def check_in(request):
-    if request.POST:
+class GetQRCodeAdmin(APIView):
+    permission_classes = (IsAdminUser,)
+    def get(self, request):
+        email = request.GET["email"]
         try:
-            uuid = request.POST["uuid"]
-        except KeyError:
-            return error_response("Must include uuid.",
-                                  "You must include the user's uuid from the bar code in the request body.",
-                                  400)
-        try:
-            group = CheckInGroup.objects.get(uuid=uuid)
-        except CheckInGroup.DoesNotExist:
-            return error_response("uuid does not exist",
-                                  "The uuid you specified does not exist in the database.",
+            applicant = Applicant.objects.get(email=email)
+        except Applicant.DoesNotExist:
+            return error_response("User does not exist.",
+                                  "There is no user with this name in the database.",
                                   404)
-        if group.checked_in:
-            return error_response("User already checked in.",
-                                  "The user specified is already checked in.",
-                                  409)
-        group.checked_in = True
-        group.save()
-        event = CheckInEvent(check_in_group=group, check_in=True, time=timezone.now())
-        event.save()
-        return success_data_jsonify()
-    else:
-        return error_response("Invalid method.", "Please use a post request.", 405)
-    # TODO: will need to be authenticated to admins
+        # TODO: filter to only include admitted applicants
+        try:
+            group = CheckInGroup.objects.get(applicant=applicant)
+        except CheckInGroup.DoesNotExist:
+            group = CheckInGroup(applicant=applicant)
+            group.save()
+        return HttpResponse(qrcode.make(group.uuid), content_type="image/png")
 
 
-@csrf_exempt
-def check_out(request):
-    if request.POST:
+
+class CheckIn(APIView):
+    permission_classes = (IsAdminUser,)
+    def post(self, request):
         if request.POST:
             try:
                 uuid = request.POST["uuid"]
@@ -87,18 +58,49 @@ def check_out(request):
                 return error_response("uuid does not exist",
                                       "The uuid you specified does not exist in the database.",
                                       404)
-            if not group.checked_in:
-                return error_response("User is not checked in.",
-                                      "A user must be checked in to be checked out.",
+            if group.checked_in:
+                return error_response("User already checked in.",
+                                      "The user specified is already checked in.",
                                       409)
-            event = CheckInEvent(check_in_group=group, check_in=False, time=timezone.now())
-            group.checked_in = False
-            event.save()
+            group.checked_in = True
             group.save()
+            event = CheckInEvent(check_in_group=group, check_in=True, time=timezone.now())
+            event.save()
             return success_data_jsonify()
         else:
             return error_response("Invalid method.", "Please use a post request.", 405)
         # TODO: will need to be authenticated to admins
+
+
+class CheckOut(APIView):
+    permission_classes = (IsAdminUser,)
+    def post(self, request):
+        if request.POST:
+            if request.POST:
+                try:
+                    uuid = request.POST["uuid"]
+                except KeyError:
+                    return error_response("Must include uuid.",
+                                          "You must include the user's uuid from the bar code in the request body.",
+                                          400)
+                try:
+                    group = CheckInGroup.objects.get(uuid=uuid)
+                except CheckInGroup.DoesNotExist:
+                    return error_response("uuid does not exist",
+                                          "The uuid you specified does not exist in the database.",
+                                          404)
+                if not group.checked_in:
+                    return error_response("User is not checked in.",
+                                          "A user must be checked in to be checked out.",
+                                          409)
+                event = CheckInEvent(check_in_group=group, check_in=False, time=timezone.now())
+                group.checked_in = False
+                event.save()
+                group.save()
+                return success_data_jsonify()
+            else:
+                return error_response("Invalid method.", "Please use a post request.", 405)
+            # TODO: will need to be authenticated to admins
 
 
 # TODO: move the below functions to utils somewhere so everyone can use them
