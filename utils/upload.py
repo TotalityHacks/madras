7f870ef6670b57789902fd5bb635bd2d22792524
yohanstra <jreinstra@gmail.com>
2018-05-14 23:28:09 -0700

    use env vars

--- utils/upload.py
+++ utils/upload.py
@@ -1,17 +1,15 @@
 import boto3
 import uuid
 
-AWS_REGION = "us-west-1"
-AWS_SERVER_PUBLIC_KEY = "AKIAI2JGOKGMGPKQVMPQ"
-AWS_SERVER_SECRET_KEY = "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"
-S3_BUCKET_NAME = "totalityhacks"
+from django.conf import settings
 
 
 class FileUploader:
 
     def __init__(
-            self, aws_key=AWS_SERVER_PUBLIC_KEY,
-            aws_secret=AWS_SERVER_SECRET_KEY, aws_region=AWS_REGION):
+            self, aws_key=settings.AWS_SERVER_PUBLIC_KEY,
+            aws_secret=settings.AWS_SERVER_SECRET_KEY,
+            aws_region=settings.AWS_REGION):
         self._s3 = boto3.client(
             "s3",
             aws_access_key_id=aws_key,
@@ -20,8 +18,8 @@ class FileUploader:
         )
 
     def upload_file_to_s3(
-            self, local_filename, s3_bucket=S3_BUCKET_NAME,
+            self, local_filename, s3_bucket=settings.AWS_S3_BUCKET_NAME,
             remote_filename=None):
         if not remote_filename:
             remote_filename = str(uuid.uuid4())
-        self._s3.upload_file(local_filename, S3_BUCKET_NAME, remote_filename)
+        self._s3.upload_file(local_filename, s3_bucket, remote_filename)
