4fc08db60d1644b991291881c44453a5326449bd
yohanstra <jreinstra@gmail.com>
2018-05-25 19:31:44 -0700

    fix resume upload

--- utils/upload.py
+++ utils/upload.py
@@ -1,4 +1,5 @@
 import boto3
+import io
 import uuid
 
 from django.conf import settings
@@ -23,3 +24,9 @@ class FileUploader:
         if not remote_filename:
             remote_filename = str(uuid.uuid4())
         self._s3.upload_fileobj(local_file, s3_bucket, remote_filename)
+
+    def download_file_from_s3(
+            self, filename, s3_bucket=settings.AWS_S3_BUCKET_NAME):
+        output = io.BytesIO()
+        self._s3.download_fileobj(s3_bucket, filename, output)
+        return output.getvalue()
