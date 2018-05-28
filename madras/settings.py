cbfbb7efb780ecd53c9f3a4f998470f9097840d9
yohanstra <jreinstra@gmail.com>
2018-05-28 15:11:35 -0700

    go right to login from confirm

--- madras/settings.py
+++ madras/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # URL of the page to redirect to after a new user has confirmed their email
-EMAIL_REDIRECT_URL = "https://apply.totalityhacks.com/"
+EMAIL_REDIRECT_URL = "https://apply.totalityhacks.com/#login"
 
 
 # Quick-start development settings - unsuitable for production
