7fc7d916c348b3156e86ff4ee85e2df50661e6eb
yohanstra <jreinstra@gmail.com>
2018-05-30 21:33:02 -0700

    Resume retrieve returns any resume and remove list view

--- apps/application/views.py
+++ apps/application/views.py
@@ -39,21 +39,18 @@ def home(request):
 
 
 class ResumeViewSet(mixins.CreateModelMixin,
-                    mixins.ListModelMixin,
                     mixins.RetrieveModelMixin,
                     viewsets.GenericViewSet):
 
     serializer_class = serializers.ResumeSerializer
     permission_classes = (IsAuthenticated,)
+    queryset = Resume.objects.all()
 
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def get_queryset(self):
-        return Resume.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         file = serializer.validated_data['file']
