f5571972563c5aafcc77af98eb856117d98b0687
thomas <thomas.woodside@gmail.com>
2018-06-05 17:48:46 -0700

    finished get events

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,12 +1,21 @@
 from apps.events.models import Event
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from dateutil.parser import parse
 
 
 @api_view(['GET'])
 def get_events(request):
+    params = request.GET
+    events = Event.objects.all()
+    if "category_id" in params:
+        events = events.filter(category=params["category_id"])
+    if "start_time" in params:
+        events = events.filter(start_time__gt=parse(params["start_time"]))
+    if "end_time" in params:
+        events = events.filter(start_time__lt=parse(params["end_time"]))
     event_array = []
-    for event in Event.objects.all():
+    for event in events:
         event_array.append(event.dictionary_representation())
     return success_data_jsonify(event_array)
 
@@ -21,7 +30,6 @@ def success_data_jsonify(data, code=200):
 
 
 def error_response(message, code):
-
 
     response = Response({
         "success": False,
