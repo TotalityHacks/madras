9d94b1199cbbc0a04fe25fbd20195de6675aa2a0
thomas <thomas.woodside@gmail.com>
2018-06-05 17:34:42 -0700

    fixed some issues with the get events view

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from apps.events.views import get_events
 
 urlpatterns = [
-    url(r'', get_events, name="get_events"),
+    url(r'', get_events, name="get_events")
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -1,14 +1,19 @@
 from apps.events.models import Event
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def get_events(request):
-    return success_data_jsonify(Event.objects.all())
+    event_array = []
+    for event in Event.objects.all():
+        event_array.append(event.dictionary_representation())
+    return success_data_jsonify(event_array)
 
 
 def success_data_jsonify(data, code=200):
     response = Response({
-        "success": True, data: "data", "error": None
+        "success": True, "data": data, "error": None
     })
     response.status_code = code
 
