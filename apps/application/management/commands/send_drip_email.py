e110a5b63c4b34fc3f0f652e222e7407fc5b9a24
Eric Wang <ezwang1@gmail.com>
2018-06-06 23:38:29 -0400

    add email to send

--- apps/application/management/commands/send_drip_email.py
+++ apps/application/management/commands/send_drip_email.py
@@ -4,6 +4,8 @@ from django.core.management.base import BaseCommand
 from django.db.models import Count
 from django.utils import timezone
 from django.conf import settings
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 from apps.registration.models import User
 
@@ -12,7 +14,8 @@ class Command(BaseCommand):
     help = "Sends a drip email to people who have registered but not applied."
 
     def handle(self, *args, **kwargs):
-        time_threshold = timezone.now() - timedelta(days=settings.DRIP_EMAIL_DAYS)
+        now = timezone.now()
+        time_threshold = now - timedelta(days=settings.DRIP_EMAIL_DAYS)
         users = User.objects.annotate(num_submissions=Count("submissions")) \
                             .filter(num_submissions=True) \
                             .filter(date_joined__lt=time_threshold) \
@@ -20,6 +23,18 @@ class Command(BaseCommand):
         num_users = users.count()
 
         for user in users:
+            message = render_to_string("drip_email.html", {
+                "user": user,
+                "registration_url": settings.EMAIL_REDIRECT_DRIP_URL
+            })
+            email = EmailMultiAlternatives(
+                "Don't forget to register for TotalityHacks!",
+                message,
+                to=[user.email]
+            )
+            email.attach_alternative(message, "text/html")
+            email.send()
+
             user.sent_drip_email = True
             user.save(update_fields=["sent_drip_email"])
 
