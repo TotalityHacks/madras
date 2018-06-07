075a75d874ccb5b83d2689cc5dd03d7d2245aeb1
thomas <thomas.woodside@gmail.com>
2018-06-06 21:11:02 -0700

    changed to longer max lengths or text fields

--- apps/announcements/models.py
+++ apps/announcements/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Announcement(models.Model):
-    message = models.CharField(max_length=200)
+    message = models.TextField()
     time = models.DateTimeField()
 
     def dictionary_representation(self):
--- apps/events/models.py
+++ apps/events/models.py
@@ -15,8 +15,8 @@ class Category(models.Model):
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=20)
-    description = models.CharField(max_length=200)
+    title = models.CharField(max_length=40)
+    description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.ForeignKey(Location)
