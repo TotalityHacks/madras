fcffe91286c7a3bc510c4767f951398883ea08b5
Eric Wang <ezwang1@gmail.com>
2018-06-08 08:17:26 -0400

    fix the minor issues

--- madras/settings.py
+++ madras/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # URL of the page to redirect to after a new user has confirmed their email
 EMAIL_REDIRECT_URL = "https://apply.totalityhacks.com/#login"
 EMAIL_REDIRECT_FAILURE_URL = "https://apply.totalityhacks.com/#resend"
-EMAIL_REDIRECT_DRIP_URL = "https://apply.totalityhacks.com/"
 
 
 # Quick-start development settings - unsuitable for production
