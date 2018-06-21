7df07a3596fc49d5ab23dbabf41abf547e763a53
Theo B <me@theos.space>
2018-06-21 01:18:00 -0400

    fix via revert

--- utils/email.py
+++ utils/email.py
@@ -25,7 +25,10 @@ def send_template_email(to_array, subject, template, context, sent_by=None):
 
 def send_delayed_email(to_array, subject, template, context, hours, sent_by=None):
     # hours = hours until send (self-explanatory)
-    header = SMTPAPIHeader().set_send_at(int(timezone.now() + datetime.timedelta(hours=hours)).timestamp())
+    header = SMTPAPIHeader()
+    delay = datetime.timedelta(hours=settings.INTRO_EMAIL_DELAY)
+    send_at = timezone.now() + delay
+    header.set_send_at(int(send_at.timestamp()))
     body = render_to_string(template, context)
     if sent_by != None:
         email = EmailMultiAlternatives(
