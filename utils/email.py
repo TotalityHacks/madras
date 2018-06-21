03bbeba42706842c816b85fe8b12b8236cd7b791
Theo B <me@theos.space>
2018-06-21 00:58:23 -0400

    Fix optional arg

--- utils/email.py
+++ utils/email.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 from smtpapi import SMTPAPIHeader
 import datetime
 
-def send_template_email(to_array, subject, template, context, sent_by):
+def send_template_email(to_array, subject, template, context, sent_by=None):
     body = render_to_string(template, context)
-    if sent_by:
+    if sent_by != None:
         email = EmailMultiAlternatives(
             mail_subject,
             message,
@@ -23,11 +23,11 @@ def send_template_email(to_array, subject, template, context, sent_by):
     email.attach_alternative(message, "text/html")
     email.send()
 
-def send_delayed_email(to_array, subject, template, context, hours, sent_by):
+def send_delayed_email(to_array, subject, template, context, hours, sent_by=None):
     # hours = hours until send (self-explanatory)
     header = SMTPAPIHeader().set_send_at(timezone.now() + datetime.timedelta(hours=hours))
     body = render_to_string(template, context)
-    if sent_by:
+    if sent_by != None:
         email = EmailMultiAlternatives(
             mail_subject,
             message,
