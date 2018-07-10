5360104d31cd15811cdf4ee54c6ced6e53be8760
Michael Abelar <miabelar@fb.com>
2018-07-09 20:48:42 -0700

    update model

--- apps/application/models.py
+++ apps/application/models.py
@@ -90,7 +90,7 @@ class Application(models.Model):
     race_ethnicity = models.CharField(
         max_length=32, choices=RACE_ETHNICITY_CHOICES, null=True, blank=True)
 
-    priority = models.BooleanField(initial=False)
+    priority = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
