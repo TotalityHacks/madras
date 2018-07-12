d13c4f8be47f7e56a6566f166ce463c200988e39
Eric Wang <ezwang1@gmail.com>
2018-07-12 12:19:23 -0400

    fix line length

--- apps/application/permissions.py
+++ apps/application/permissions.py
@@ -4,4 +4,5 @@ from rest_framework.permissions import IsAuthenticated
 class IsAuthenticatedOrViewing(IsAuthenticated):
 
     def has_permission(self, request, view):
-        return view.action == 'retrieve' or super(IsAuthenticatedOrViewing, self).has_permission(request, view)
+        return view.action == 'retrieve' or super(
+            IsAuthenticatedOrViewing, self).has_permission(request, view)
